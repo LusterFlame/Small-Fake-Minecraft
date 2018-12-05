1de92f15bb13ddbe905dfb2066afa30d1d39027d
LusterFlame_Laptop <kurasiki.homura@gmail.com>
2018-12-05 11:15:42 +0800

    Stone added

--- Small Fake Minecraft/Assets/Script/GroundGeneration.cs
+++ Small Fake Minecraft/Assets/Script/GroundGeneration.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GroundGeneration : MonoBehaviour {
@@ -7,6 +8,7 @@ public class GroundGeneration : MonoBehaviour {
 	public GameObject bedrock;
 	public GameObject grass;
 	public GameObject dirt;
+	public GameObject stone;
 	private GameObject bedrockCollider;
 
 	// Use this for initialization
@@ -17,54 +19,36 @@ public class GroundGeneration : MonoBehaviour {
 		bedrockCollider.AddComponent<BoxCollider>();
 		bedrockCollider.GetComponent<BoxCollider>().center = new Vector3(0, 0, 0);
 		bedrockCollider.GetComponent<BoxCollider>().size = new Vector3(64, 1, 64);
-		//Debug.Log(Bedrock_collider.GetComponent<BoxCollider>().center);
-		for (int temp = -31; temp <= 32; ++temp)
-			for (int temp2 = -31; temp2 <= 32; ++temp2)
-			{
-				GameObject block = Instantiate(bedrock);
-				Destroy(block.GetComponent<BoxCollider>());
-				block.GetComponent<Transform>().position = new Vector3(temp, 0, temp2);
-			}
+		placeBedrock();
 
 		//Decide Ground height for every block
 		int[,] height = new int[64, 64];
 		setHeight(height);
 		// Original Height Decided
-		// But not flat enough
 
 		//place dirt blocks
-		for(int temp = -31;temp <= 32;++temp)
-			for(int temp2 = -31;temp2 <= 32;++temp2)
-			{
-				for (int temp3 = 1; temp3 < height[temp + 31, temp2 + 31];++temp3)
-				{
-					GameObject block2 = Instantiate(dirt);
-					block2.transform.position = new Vector3(temp, temp3, temp2);
-				}
-				GameObject block3 = Instantiate(grass);
-				block3.transform.position = new Vector3(temp, height[temp + 31, temp2 + 31], temp2);
-			}
+		placeGround(height);
 
 
 	}
 
-	private void setHeight(int[,] Height)
+	private void setHeight(int[,] height)
 	{
-		Height[0, 0] = Random.Range(1, 4);
+		height[0, 0] = Random.Range(1, 4);
 		for (int temp = 0; temp < 64; temp++)
 			for (int temp2 = 0; temp2 < 64; temp2++)
 			{
-				if (Height[temp, temp2] == 0)
+				if (height[temp, temp2] == 0)
 				{
 					if (temp == 0)
 					{
-						switch (Random.Range(0, 10))
+						switch (Random.Range(0, 15))
 						{
 							case 0:
-								if (!(Height[temp, temp2 - 1] == 1))
-									Height[temp, temp2] = Height[temp, temp2 - 1] - 1;
+								if (!(height[temp, temp2 - 1] == 1))
+									height[temp, temp2] = height[temp, temp2 - 1] - 1;
 								else
-									Height[temp, temp2] = Height[temp, temp2 - 1];
+									height[temp, temp2] = height[temp, temp2 - 1];
 								break;
 							case 1:
 							case 2:
@@ -74,22 +58,27 @@ public class GroundGeneration : MonoBehaviour {
 							case 6:
 							case 7:
 							case 8:
-								Height[temp, temp2] = Height[temp, temp2 - 1];
-								break;
 							case 9:
-								Height[temp, temp2] = Height[temp, temp2 - 1] + 1;
+							case 10:
+							case 11:
+							case 12:
+							case 13:
+								height[temp, temp2] = height[temp, temp2 - 1];
+								break;
+							case 14:
+								height[temp, temp2] = height[temp, temp2 - 1] + 1;
 								break;
 						}
 					}
 					else if (temp2 == 0)
 					{
-						switch (Random.Range(0, 10))
+						switch (Random.Range(0, 15))
 						{
 							case 0:
-								if (!(Height[temp - 1, temp2] == 1))
-									Height[temp, temp2] = Height[temp - 1, temp2] - 1;
+								if (!(height[temp - 1, temp2] == 1))
+									height[temp, temp2] = height[temp - 1, temp2] - 1;
 								else
-									Height[temp, temp2] = Height[temp - 1, temp2];
+									height[temp, temp2] = height[temp - 1, temp2];
 								break;
 							case 1:
 							case 2:
@@ -99,42 +88,47 @@ public class GroundGeneration : MonoBehaviour {
 							case 6:
 							case 7:
 							case 8:
-								Height[temp, temp2] = Height[temp - 1, temp2];
-								break;
 							case 9:
-								Height[temp, temp2] = Height[temp - 1, temp2] + 1;
+							case 10:
+							case 11:
+							case 12:
+							case 13:
+								height[temp, temp2] = height[temp - 1, temp2];
+								break;
+							case 14:
+								height[temp, temp2] = height[temp - 1, temp2] + 1;
 								break;
 						}
 					}
 					else
 					{
-						int Range = Height[temp - 1, temp2] - Height[temp, temp2 - 1];
+						int Range = height[temp - 1, temp2] - height[temp, temp2 - 1];
 						switch (Range)
 						{
 							case 2:
 							case -2:
-								Height[temp, temp2] = (Height[temp - 1, temp2] + Height[temp, temp2 - 1]) / 2;
+								height[temp, temp2] = (height[temp - 1, temp2] + height[temp, temp2 - 1]) / 2;
 								break;
 							case 1:
 							case -1:
 								switch (Random.Range(0, 2))
 								{
 									case 0:
-										Height[temp, temp2] = Height[temp - 1, temp2];
+										height[temp, temp2] = height[temp - 1, temp2];
 										break;
 									case 1:
-										Height[temp, temp2] = Height[temp, temp2 - 1];
+										height[temp, temp2] = height[temp, temp2 - 1];
 										break;
 								}
 								break;
 							case 0:
-								switch (Random.Range(0, 10))
+								switch (Random.Range(0, 15))
 								{
 									case 0:
-										if (!(Height[temp - 1, temp2] == 1))
-											Height[temp, temp2] = Height[temp - 1, temp2] - 1;
+										if (!(height[temp - 1, temp2] == 1))
+											height[temp, temp2] = height[temp - 1, temp2] - 1;
 										else
-											Height[temp, temp2] = Height[temp - 1, temp2];
+											height[temp, temp2] = height[temp - 1, temp2];
 										break;
 									case 1:
 									case 2:
@@ -144,10 +138,15 @@ public class GroundGeneration : MonoBehaviour {
 									case 6:
 									case 7:
 									case 8:
-										Height[temp, temp2] = Height[temp - 1, temp2];
-										break;
 									case 9:
-										Height[temp, temp2] = Height[temp - 1, temp2] + 1;
+									case 10:
+									case 11:
+									case 12:
+									case 13:
+										height[temp, temp2] = height[temp - 1, temp2];
+										break;
+									case 14:
+										height[temp, temp2] = height[temp - 1, temp2] + 1;
 										break;
 								}
 								break;
@@ -155,10 +154,59 @@ public class GroundGeneration : MonoBehaviour {
 					}
 				}
 			}
-	}
-	private void setBedrock()
-	{
 
+		//Make it smoother
+		for(int temp = 0;temp < 3;++temp)
+			flattening(height);
+	}
+	private void flattening(int[,] height)
+	{
+		for (int temp = 1; temp < 63; ++temp)
+			for (int temp2 = 1; temp2 < 63; ++temp2)
+			{
+				List<int> aroundHeight = new List<int>()
+				{
+					height[temp - 1, temp2 - 1],
+					height[temp - 1, temp2],
+					height[temp - 1, temp2 + 1],
+					height[temp, temp2 - 1],
+					height[temp, temp2 + 1],
+					height[temp + 1, temp2 - 1],
+					height[temp + 1, temp2 - 1],
+					height[temp + 1, temp2 - 1],
+				};
+				if (aroundHeight.Count(x => x == height[temp, temp2]) <= 1)
+					height[temp, temp2] = aroundHeight[Random.Range(0, 8)];
+			}
+	}
+	private void placeBedrock()
+	{
+		for (int temp = -31; temp <= 32; ++temp)
+			for (int temp2 = -31; temp2 <= 32; ++temp2)
+			{
+				GameObject block = Instantiate(bedrock);
+				Destroy(block.GetComponent<BoxCollider>());
+				block.GetComponent<Transform>().position = new Vector3(temp, 0, temp2);
+			}
+	}
+	private void placeGround(int[,] height)
+	{
+		for (int temp = -31; temp <= 32; ++temp)
+			for (int temp2 = -31; temp2 <= 32; ++temp2)
+			{
+				for (int temp3 = 1; temp3 < height[temp + 31, temp2 + 31] - 1; ++temp3)
+				{
+					GameObject block2 = Instantiate(stone);
+					block2.transform.position = new Vector3(temp, temp3, temp2);
+				}
+				for (int temp3 = height[temp + 31, temp2 + 31] - 1; temp3 < height[temp + 31, temp2 + 31]; ++temp3)
+				{
+					GameObject block4 = Instantiate(dirt);
+					block4.transform.position = new Vector3(temp, temp3, temp2);
+				}
+				GameObject block3 = Instantiate(grass);
+				block3.transform.position = new Vector3(temp, height[temp + 31, temp2 + 31], temp2);
+			}
 	}
 	
 	// Update is called once per frame
